588a1854e740cbc6bde0a138f0bbbad9e513f0de
Applied Demography Toolbox <45884231+AppliedDemogToolbox@users.noreply.github.com>
2022-03-18 17:55:01 -0700

    Update BrassRelationalLogitCode.R

--- BrassRelationalLogitCode.R
+++ BrassRelationalLogitCode.R
@@ -1,7 +1,7 @@
 ##############################################################################################################################
 ##R CODE FOR THE BRASS RELATIONAL LOGIT MODEL OF MORTALITY
 ##
-##EDDIE HUNSINGER, FEBRUARY 2011 (LAST UPDATED DECEMBER 2018)
+##EDDIE HUNSINGER, FEBRUARY 2011 (LAST UPDATED MARCH 2022)
 ##http://www.demog.berkeley.edu/~eddieh/ 
 ##edyhsgr@gmail.com
 ##
@@ -18,10 +18,17 @@
 lxSample<-read.table(file="https://raw.githubusercontent.com/AppliedDemogToolbox/Hunsinger_BrassRelationalLogit/master/lxSample.csv",header=TRUE,sep=",")
 lxSSA<-read.table(file="https://raw.githubusercontent.com/AppliedDemogToolbox/Hunsinger_BrassRelationalLogit/master/lxSSA.csv",header=TRUE,sep=",")
 
-plot(lxSample$X2000f,type="l",col="blue",lwd=5)
-mtext(line=-14,text="Sample lx",font=2,cex=1,col="blue")
-points(lxSSA$X2000f,type="l",col="red",lwd=5)
-mtext(line=-10,text="SSA/Standard lx",font=2,cex=1,col="red")
+plot(lxSample$X2000f,type="l",
+main="Brass relational logit model demonstration",
+		xlab="age",
+		ylab="survivorship",
+		col="blue",lwd=5,axes=FALSE)
+	axis(side=2,las=2)
+	axis(side=1,at=1:length(lxSample$x),
+		labels=lxSample$x,las=2)
+points(lxSSA$X2000f,type="l",lty=2,col="red",lwd=5)
+legend(x="left",legend=c("sample","standard"),
+       col=c("blue","red"),lty=c(1,2),lwd=c(5,5),bg="white")
 
 ##############################################################################################################################
 #STEP 2: Enter the Brass Relational Logit function
@@ -52,8 +59,9 @@ for (i in 1:length(lxSSA$X2000f)){Yx[i]<-.5*log(lxSSA$X2000f[i]/(1-lxSSA$X2000f[
 lxSSAAdjusted<-NULL
 for (i in 1:length(lxSSA$X2000f)){lxSSAAdjusted[i]<-1/(1+exp(-2*BrassCoefficients$Alpha-2*BrassCoefficients$Beta*Yx[i]))}
 
-points(lxSSAAdjusted,type="l",col="purple",lwd=5)
-mtext(line=-12,text="SSA lx Adjusted",font=2,cex=1,col="purple")
+points(lxSSAAdjusted,type="l",lty=3,col="purple",lwd=5)
+legend(x="left",legend=c("sample","standard","adjusted standard"),
+       col=c("blue","red","purple"),lty=c(1,2,3),lwd=c(5,5,5),bg="white")
 
 ##############################################################################################################################
 #STEP 5: Plot some effects of change in Brass Alpha and Brass Beta
@@ -69,19 +77,32 @@ for (i in 1:length(lxSSA$X2000f)){lxSSAHighBeta[i]<-1/(1+exp(-2*(0)-2*(1.5)*Yx[i
 lxSSALowBeta<-NULL
 for (i in 1:length(lxSSA$X2000f)){lxSSALowBeta[i]<-1/(1+exp(-2*(0)-2*(.5)*Yx[i]))}
 
-plot(lxSSA$X2000f,type="l",col="purple",lwd=5)
-mtext(line=-12,text="SSA Standard lx",font=2,cex=1,col="purple")
-points(lxSSAHighAlpha,type="l",col="red",lwd=5)
-mtext(line=-10,text="SSA High (0.5) Alpha lx",font=2,cex=1,col="red")
-points(lxSSALowAlpha,type="l",col="blue",lwd=5)
-mtext(line=-14,text="SSA Low (-0.5) Alpha lx",font=2,cex=1,col="blue")
+plot(lxSSA$X2000f,type="l",
+main="Brass relational logit model demonstration",
+		xlab="age",
+		ylab="survivorship",
+		col="purple",lwd=5,axes=FALSE)
+	axis(side=2,las=2)
+	axis(side=1,at=1:length(lxSample$x),
+		labels=lxSample$x,las=2)
+points(lxSSAHighAlpha,type="l",lty=2,col="red",lwd=5)
+points(lxSSALowAlpha,type="l",lty=3,col="blue",lwd=5)
+legend(x="left",legend=c("standard","standard with 0.5 alpha","standard with -0.5 alpha"),
+       col=c("purple","red","blue"),lty=c(1,2,3),lwd=c(5,5,5),bg="white")
 
-plot(lxSSA$X2000f,type="l",col="purple",lwd=5)
-mtext(line=-12,text="SSA Standard lx",font=2,cex=1,col="purple")
-points(lxSSAHighBeta,type="l",col="red",lwd=5)
-mtext(line=-10,text="SSA High (1.5) Beta lx",font=2,cex=1,col="red")
-points(lxSSALowBeta,type="l",col="blue",lwd=5)
-mtext(line=-14,text="SSA Low (0.5) Beta lx",font=2,cex=1,col="blue")
+plot(lxSSA$X2000f,type="l",
+main="Brass relational logit model demonstration",
+		xlab="age",
+		ylab="survivorship",
+		col="purple",lwd=5,axes=FALSE)
+	axis(side=2,las=2)
+	axis(side=1,at=1:length(lxSample$x),
+		labels=lxSample$x,las=2)
+points(lxSSAHighBeta,type="l",lty=2,col="red",lwd=5)
+points(lxSSALowBeta,type="l",lty=3,col="blue",lwd=5)
+legend(x="left",legend=c("standard","standard with 0.5 beta","standard with -0.5 beta"),
+       col=c("purple","red","blue"),lty=c(1,2,3),lwd=c(5,5,5),bg="white")
 
 #write.table(###, file="G:/###/###.csv", sep=",")
+
 
